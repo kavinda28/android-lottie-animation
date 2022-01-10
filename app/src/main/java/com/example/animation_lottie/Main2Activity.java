423061a2433ff359db23f9d45c2a0ad851f05e2b
kavindaM <kavindamaduranga822@gmail.com>
2022-01-10 12:40:55 +0530

    Lottie-Animation-And-Splash-Screen

---
+++ app/src/main/java/com/example/animation_lottie/Main2Activity.java
@@ -0,0 +1,30 @@
+package com.example.animation_lottie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class Main2Activity extends AppCompatActivity {
+    LottieAnimationView lottieAnimationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+
+        lottieAnimationView=findViewById(R.id.lottie);
+        lottieAnimationView.animate().translationY(1600).setDuration(1000).setStartDelay(4000);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent myIntent = new Intent(Main2Activity.this, MainActivity.class);
+                startActivity(myIntent);
+            }
+        },5000);
+    }
+}
